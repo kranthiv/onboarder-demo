5322c7754bb7653fd0af79e0fc3c313a0a40dbc6
kranthi veeramachaneni <kranthi1056@gmail.com>
2017-05-29 23:08:14 +0100

    demo chnages

--- Onboarder.js
+++ Onboarder.js
@@ -1,13 +1,16 @@
+
 function OnBoard(data) {
     let journey = {};
     journey.id = data.id;
+    journey.showPrevButton = true,
     journey.steps = [];
     data.steps.forEach((step) => {
         journey.steps.push({
             title: step.title,
             content: step.content,
             target: document.querySelector(step.target),
-            placement: step.placement
+            placement: step.placement,
+            showSkip : data.showSkip || false
         })
     });
     return journey;
